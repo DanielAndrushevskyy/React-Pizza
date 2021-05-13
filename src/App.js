76c5a36d2eab23fe0297ceca1b2d6b347bc21caa
DanielAndrushevskyy <andrushevskyy07@gamil.com>
2021-05-14 02:49:40 +0300

    Test 6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { Route } from 'react-router';
 import { Header } from './components';
 import { Home, Cart } from './pages';
@@ -7,11 +9,9 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
   
   React.useEffect(() => {
-  fetch('http://localhost:3000/db.json')
-    .then((resp) => resp.json())
-    .then((json) => {
-      setPizzas(json.pizzas);
-    });
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+  });
   }, []);
 
   return (
