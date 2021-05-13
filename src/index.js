97ae3ec63a1f93e1d7eff66cb16a3f1b3df8dfb3
DanielAndrushevskyy <andrushevskyy07@gamil.com>
2021-05-14 02:55:04 +0300

    Test 6.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
+// import Header from './components/Header';
 
 import './scss/app.scss';
 
