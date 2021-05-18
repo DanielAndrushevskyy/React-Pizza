7516bb411b72ab37e84bfa0841ccb2e921818484
DanielAndrushevskyy <andrushevskyy07@gamil.com>
2021-05-18 23:24:43 +0300

    Test 9.2

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -14,8 +14,8 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
    axios
       .get(
          `http://localhost:3001/pizzas?${
-            category !== null ? `category=${category}` : ''}
-            &_sort=${sortBy.type}&_order=${sortBy.order}`,
+            category !== null ? `category=${category}` : ''
+      }&_sort=${sortBy.type}&_order=${sortBy.order}`,
       )
          .then(({ data }) => {
             dispatch(setPizzas(data));
